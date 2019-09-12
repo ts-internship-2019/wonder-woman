baf047f080e3358d2280d016121d272e0e3c89b2
Andrei Martoiu <amartoiu@totalsoft.ro>
2019-09-12 12:20:32 +0300

    test for commit Andrei 2019

--- iWasHere.Web/Program.cs
+++ iWasHere.Web/Program.cs
@@ -15,6 +15,7 @@ namespace iWasHere.Web
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            string something = "I was here";
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
