f2a0a58112268747f1706b509693dbff98fcab1d
crash10degeaba <amartoiu@totalsoft.ro>
2019-09-17 17:41:30 +0300

    TI-25 Fixed Filtering but still the UI is not working

--- iWasHere.Domain/Service/DictionaryService.cs
+++ iWasHere.Domain/Service/DictionaryService.cs
@@ -31,27 +31,53 @@ namespace iWasHere.Domain.Service
             return dictionaryLandmarkTypeModels;
         } 
         public List<CityModel> GetAllPagedCities(int skipRows, int pageSize, string filterName, int filterCounty, out int totalRows)
-        {           
-            var query = _dbContext.City.Where(a => a.Name.Contains(filterName)).Include(b => b.County).Where(b => b.CountyId.Equals(filterCounty));
+        {
             totalRows = 0;
-            if(query.Count() > 0)
+            if (filterCounty > 0)
             {
-                var page = query.OrderBy(p => p.Name)
-                            .Select(p => new CityModel()
-                            {
-                                Id = p.CityId,
-                                Name = p.Name,
-                                Code = p.Code,
-                                CountyId = p.CountyId,
-                                CountyName = p.County.Name
-                            })
-                            .Skip(skipRows).Take(pageSize)
-                            .GroupBy(p => new { Total = query.Count() })
-                            .First();
-                totalRows = page.Key.Total;
-                var cities = page.Select(p => p);
-                return cities.ToList();
+                var query = _dbContext.City.Where(a => a.Name.Contains(filterName)).Include(b => b.County).Where(b => b.CountyId.Equals(filterCounty));
+                if (query.Count() > 0)
+                {
+                    var page = query.OrderBy(p => p.CityId)
+                                .Select(p => new CityModel()
+                                {
+                                    Id = p.CityId,
+                                    Name = p.Name,
+                                    Code = p.Code,
+                                    CountyId = p.CountyId,
+                                    CountyName = p.County.Name
+                                })
+                                .Skip(skipRows).Take(pageSize)
+                                .GroupBy(p => new { Total = query.Count() })
+                                .First();
+                    totalRows = page.Key.Total;
+                    var cities = page.Select(p => p);
+                    return cities.ToList();
+                }
             }
+            else
+            {
+                var query = _dbContext.City.Where(a => a.Name.Contains(filterName)).Include(b => b.County);
+                if (query.Count() > 0)
+                {
+                    var page = query.OrderBy(p => p.CityId)
+                                .Select(p => new CityModel()
+                                {
+                                    Id = p.CityId,
+                                    Name = p.Name,
+                                    Code = p.Code,
+                                    CountyId = p.CountyId,
+                                    CountyName = p.County.Name
+                                })
+                                .Skip(skipRows).Take(pageSize)
+                                .GroupBy(p => new { Total = query.Count() })
+                                .First();
+                    totalRows = page.Key.Total;
+                    var cities = page.Select(p => p);
+                    return cities.ToList();
+                }
+            }
+            
             return new List<CityModel>();            
         }
         public List<CountyModel> GetCounties()
