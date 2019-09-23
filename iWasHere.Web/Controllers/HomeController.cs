114d9e6a9f06041d0e4ec0d1e8be8fe41d9abe1b
Daiana Paunescu <dpaunescu@totalsoft.ro>
2019-09-23 13:24:47 +0300

    TI-32 New Page

--- iWasHere.Web/Controllers/HomeController.cs
+++ iWasHere.Web/Controllers/HomeController.cs
@@ -5,11 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using iWasHere.Web.Models;
+using iWasHere.Domain.Models;
+using iWasHere.Domain.Service;
+using iWasHere.Domain.DTOs;
 
 namespace iWasHere.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HomeService _homeService;
+
+        public HomeController(HomeService homeService)
+        {
+            _homeService = homeService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -24,6 +34,33 @@ namespace iWasHere.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //updatebutton
+        [HttpPost]
+        public IActionResult LandmarkSubmit(LandmarkModel landmark, string btnSave)
+        {
+            switch (btnSave)
+            {
+                case "Save":
+                    _homeService.UpdateLandmark(landmark, out string errorMessage2);
+                    if (!string.IsNullOrEmpty(errorMessage2))
+                    {
+                        TempData["message"] = errorMessage2;
+                        return RedirectToAction("AddEditNewLandmark", new { id = landmark.LandmarkId });
+                    }
+                    return Redirect("/Dictionary/Landmark");
+                case "Save and New":
+                    _homeService.UpdateLandmark(landmark, out string errorMessage);
+                    if (!string.IsNullOrEmpty(errorMessage))
+                    {
+                        TempData["message"] = errorMessage;
+                        return RedirectToAction("AddEditNewLandmark", new { id = landmark.LandmarkId });
+                    }
+                    return Redirect("/Dictionary/Landmark");
+                default:
+                    return Redirect("/Dictionary/Landmark");
+            }
         }
     }
 }
