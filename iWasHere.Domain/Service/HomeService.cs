49b2eab748d9ade44c3a320167b34b4b538f4e39
Razvan Boerescu <rboerescu@totalsoft.ro>
2019-09-23 11:27:09 +0300

    added HomeService

---
+++ iWasHere.Domain/Service/HomeService.cs
@@ -0,0 +1,22 @@
+﻿using iWasHere.Domain.DTOs;
+using iWasHere.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace iWasHere.Domain.Service
+{
+    public class HomeService
+    {
+        private readonly DatabaseContext _dbContext;
+        public HomeService(DatabaseContext databaseContext)
+        {
+            _dbContext = databaseContext;
+        }
+
+    }
+}
--- iWasHere.Web/Startup.cs
+++ iWasHere.Web/Startup.cs
@@ -50,6 +50,7 @@ namespace iWasHere.Web
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddKendo();
             services.AddScoped<DictionaryService>();
+            services.AddScoped<HomeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
