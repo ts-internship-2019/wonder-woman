4fed553ccdc31661b88de0f4d3d3f698a12068be
Daniel Stan <dstan@totalsoft.ro>
2019-09-23 11:35:19 +0300

    Added LandmarkModel to use in Landmark_Read

---
+++ iWasHere.Domain/DTOs/LandmarkModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iWasHere.Domain.DTOs
+{
+    public class LandmarkModel
+    {
+        public int LandmarkId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int? ConstructionTypeId { get; set; }
+        public int? HistoricalPeriodTypeId { get; set; }
+        public int? LandmarkTypeId { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public int? CountryId { get; set; }
+        public int? CountyId { get; set; }
+        public int? CityId { get; set; }
+        public string MapUrl { get; set; }
+    }
+}
