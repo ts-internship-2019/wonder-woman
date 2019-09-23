7eb5d203a2dcc5a777bd8c552c8a00469e48c330
Daniel Stan <dstan@totalsoft.ro>
2019-09-23 13:04:26 +0300

    google maps testing url

--- iWasHere.Web/Controllers/HomeController.cs
+++ iWasHere.Web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using iWasHere.Web.Models;
+using iWasHere.Domain.DTOs;
 
 namespace iWasHere.Web.Controllers
 {
@@ -18,6 +19,13 @@ namespace iWasHere.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Landmark_Read()
+        {
+            LandmarkModel model = new LandmarkModel();
+            model.MapUrl = "https://www.google.com/maps/embed/v1/directions?origin=1%20Foxfield%20Lawn2C%20Raheny%2C%20Ireland&destination=128%20Old%20County%20Road%2C%20Crumlin%2C%20Ireland&key=AIzaSyC0vB7-K0LOaHIDEGEgHba6Wo2f099UFvE";
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
